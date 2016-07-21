46badd288fa2fa6f18929d55177ebe073497ab34
jahnavikoppisetty <jahnavikoppisetty@gmail.com>
2016-07-21 03:44:23 -0300

    Update Account.java

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -31,24 +31,6 @@ public abstract class Account {
 	public BigDecimal interestEarned() {
 		BigDecimal amount = sumTransactions();
 		return calculateInterest(amount);
-		// switch(accountType){
-		// case SAVINGS:
-		// if (amount <= 1000)
-		// return amount * 0.001;
-		// else
-		// return 1 + (amount-1000) * 0.002;
-		//// case SUPER_SAVINGS:
-		//// if (amount <= 4000)
-		//// return 20;
-		// case MAXI_SAVINGS:
-		// if (amount <= 1000)
-		// return amount * 0.02;
-		// if (amount <= 2000)
-		// return 20 + (amount-1000) * 0.05;
-		// return 70 + (amount-2000) * 0.1;
-		// default:
-		// return amount * 0.001;
-		// }
 	}
 
 	public BigDecimal sumTransactions() {
